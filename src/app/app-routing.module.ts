aa0c988c4f853db1d9b162274f7d01a67b5df1b3
jaisurya dev singh <jdsingh7@myseneca.ca>
2021-04-23 06:52:22 -0400

    yes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import {LoginComponent} from './login/login.component'
 
 
-const routes: Routes = [
-  {path: '/login', component: LoginComponent},
-  {path: '***',component: AppComponent}
-]
+
+const routes: Routes = []
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
